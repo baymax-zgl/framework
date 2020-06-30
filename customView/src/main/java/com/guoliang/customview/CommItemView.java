3e80cc5f278141de5a68fc6c0ed23ad5f816d1d9
张国梁 <zhangguoliang@hudunsoft.com>
2020-06-30 11:03:28 +0800

    添加自绘View模块

---
+++ customView/src/main/java/com/guoliang/customview/CommItemView.java
@@ -0,0 +1,69 @@
+package com.guoliang.customview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+
+/**
+ * Created by Administrator on 2017/6/1.
+ * 我的页面选择ITEM
+ */
+
+public class CommItemView extends ConstraintLayout {
+
+    private TextView item_right;
+    private TextView title_text;
+
+    public CommItemView(Context context) {
+        super(context);
+    }
+
+    public CommItemView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        // 加载当前这个组合控件的布局文件
+        View.inflate(context, R.layout.comm_item_view, this);
+        // 获取当前组合控件中所有的元素
+        ImageView imageView = (ImageView) findViewById(R.id.item_image);
+        ImageView imgNav = (ImageView) findViewById(R.id.img_nav);
+        title_text = (TextView) findViewById(R.id.item_title);
+        item_right = (TextView) findViewById(R.id.item_right);
+        View cut_off_rule = findViewById(R.id.cut_off_rule);
+        // 加载自定义属性
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CommItemView);
+        String title = a.getString(R.styleable.CommItemView_title_text);
+        String rightText = a.getString(R.styleable.CommItemView_right_text);
+        Drawable backIcon = a.getDrawable(R.styleable.CommItemView_right_icon);
+        Drawable icon = a.getDrawable(R.styleable.CommItemView_icon);
+        boolean show_rule = a.getBoolean(R.styleable.CommItemView_show_rule, false);
+        if (show_rule) {
+            cut_off_rule.setVisibility(VISIBLE);
+        } else {
+            cut_off_rule.setVisibility(GONE);
+        }
+        if (backIcon!=null){
+            imgNav.setBackground(backIcon);
+        }else {
+            imgNav.setBackgroundResource(R.mipmap.icon_right2);
+        }
+
+        // 将组合控件中元素和自定义的属性值进行绑定
+        imageView.setImageDrawable(icon);
+        title_text.setText(title);
+        a.recycle();
+    }
+
+    public void setItem_right(String text) {
+        item_right.setText(text);
+    }
+
+    public TextView getTitle_text() {
+        return title_text;
+    }
+}
---
+++ customView/src/main/java/com/guoliang/customview/MyToolbar.java
@@ -0,0 +1,115 @@
+package com.guoliang.customview;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.core.content.ContextCompat;
+
+import com.google.android.material.appbar.AppBarLayout;
+
+/**
+ * Created by guoliang on 2018/4/27.
+ */
+
+public class MyToolbar extends AppBarLayout {
+    private ImageView right_image;
+    private ImageView left_image;
+    private TextView app_title;
+    private TextView right_text;
+    private TextView left_text;
+
+    public MyToolbar(Context context) {
+        super(context);
+    }
+
+    public MyToolbar(final Context context, AttributeSet attrs) {
+        super(context, attrs);
+        View.inflate(context, R.layout.toolbar_layout, this);
+        right_image = (ImageView) findViewById(R.id.right_image);
+        left_text = (TextView) findViewById(R.id.left_text);
+        left_image = (ImageView) findViewById(R.id.left_image);
+        app_title = (TextView) findViewById(R.id.app_title);
+        right_text = (TextView) findViewById(R.id.right_text);
+        AppBarLayout app_bar = (AppBarLayout) findViewById(R.id.app_bar);
+        // 加载自定义属性
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.MyToolbar);
+        String right_string = a.getString(R.styleable.MyToolbar_right_title);
+        String left_string = a.getString(R.styleable.MyToolbar_left_title);
+        String app_string = a.getString(R.styleable.MyToolbar_app_title);
+        boolean left_image_null = a.getBoolean(R.styleable.MyToolbar_left_image_null, false);
+        Drawable left_draw = a.getDrawable(R.styleable.MyToolbar_left_image);
+        Drawable right_draw = a.getDrawable(R.styleable.MyToolbar_right_image);
+        float left_padding = a.getDimension(R.styleable.MyToolbar_left_padding,left_image.getPaddingLeft());
+        Drawable app_bar_drawable = a.getDrawable(R.styleable.MyToolbar_app_bar_drawable);
+        int right_title_color = a.getColor(R.styleable.MyToolbar_right_title_color, Color.parseColor("#ffffff"));
+        int app_bar_color = a.getColor(R.styleable.MyToolbar_app_bar_color, Color.parseColor("#ffffff"));
+        int app_title_color = a.getColor(R.styleable.MyToolbar_app_title_color, Color.parseColor("#000000"));
+        if (app_bar_drawable==null) {
+            app_bar.setBackgroundColor(app_bar_color);
+        }else {
+            app_bar.setBackground(app_bar_drawable);
+        }
+        if (right_draw == null) right_image.setVisibility(View.GONE);
+        if (left_string == null) left_text.setVisibility(View.GONE);
+        if (right_string == null) right_text.setVisibility(View.GONE);
+        right_image.setImageDrawable(right_draw);
+        left_text.setTextColor(right_title_color);
+        right_text.setTextColor(right_title_color);
+        app_title.setTextColor(app_title_color);
+        if (left_draw == null) {
+            left_image.setImageDrawable(ContextCompat.getDrawable(context, R.mipmap.icon_fanhui_black));
+        } else {
+            left_image.setPadding((int) left_padding,0,0,0);
+            left_image.setImageDrawable(left_draw);
+        }
+        left_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Activity activity = (Activity) context;
+                activity.finish();
+            }
+        });
+        if (left_image_null) {
+            left_image.setVisibility(View.GONE);
+        }
+        app_title.setText(app_string);
+        left_text.setText(left_string);
+        right_text.setText(right_string);
+        a.recycle();
+    }
+
+    public ImageView getRight_image() {
+        return right_image;
+    }
+
+    public ImageView getLeft_image() {
+        return left_image;
+    }
+
+    public TextView getApp_title() {
+        return app_title;
+    }
+
+    public TextView getRight_text() {
+        return right_text;
+    }
+
+    public TextView getLeft_text() {
+        return left_text;
+    }
+
+    public void setApp_title(String text) {
+        app_title.setText(text);
+    }
+
+    public interface OnBackClickListener {
+        void setOnBackClickListener();
+    }
+}
