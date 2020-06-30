44ba2bb303c95a36588ff323feb9b3d76b131a80
张国梁 <zhangguoliang@hudunsoft.com>
2020-06-30 10:54:08 +0800

    优化ViewModule基类

--- frameKT/src/main/java/com/guoliang/framekt/base/BaseVMActivity.kt
+++ frameKT/src/main/java/com/guoliang/framekt/base/BaseVMActivity.kt
@@ -8,32 +8,35 @@ import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.ViewModel
 
 /**
  * @Description:
  * @Author: zhangguoliang
  * @CreateTime: 2020/6/28 9:40
  */
-abstract class BaseVMActivity<VM : BaseViewModel>(
+abstract class BaseVMActivity<VM : ViewModel,DB : ViewDataBinding>(
     useDataBinding: Boolean = true,
     hideStatusBar: Boolean = true
 ) : AppCompatActivity() {
 
     private val _useBinding = useDataBinding
     private val _hideStatusBar = hideStatusBar
-    protected lateinit var mBinding: ViewDataBinding
-    lateinit var mViewModel: VM
+    private lateinit var mBinding: DB
+    private lateinit var mViewModel: VM
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mViewModel = initVM()
+        if (initVM!=null) {
+            mViewModel = initVM!!
+        }
 
         startObserve()
         if (_useBinding) {
-            mBinding = DataBindingUtil.setContentView(this, getLayoutResId())
+            mBinding = DataBindingUtil.setContentView(this, layoutId)
             mBinding.lifecycleOwner = this
         } else {
-            setContentView(getLayoutResId())
+            setContentView(layoutId)
         }
         if (_hideStatusBar) {
             // 5.0以上系统状态栏透明
@@ -51,14 +54,13 @@ abstract class BaseVMActivity<VM : BaseViewModel>(
 
         initView()
         initData()
-        ActivityManager.instance?.addActivity(this)
     }
     override fun finish() {
         super.finish()
         ActivityManager.instance?.removeActivity(this)
     }
-    open fun getLayoutResId(): Int = 0
-    abstract fun initVM(): VM
+    abstract val layoutId: Int
+    abstract val initVM: VM?
     abstract fun initView()
     abstract fun initData()
     abstract fun startObserve()
