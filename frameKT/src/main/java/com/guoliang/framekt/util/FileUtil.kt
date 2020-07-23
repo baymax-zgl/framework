93cb18d9a69cad452337cc0a781f4056659513a8
张国梁 <zhangguoliang@hudunsoft.com>
2020-07-23 11:57:48 +0800

    锐动资源裁剪，动态下载资源

--- frameKT/src/main/java/com/guoliang/framekt/util/FileUtil.kt
+++ frameKT/src/main/java/com/guoliang/framekt/util/FileUtil.kt
@@ -1,9 +1,9 @@
 package com.guoliang.framekt.util
 
-import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.io.InputStream
+import java.io.*
+import java.util.*
+import java.util.zip.ZipEntry
+import java.util.zip.ZipFile
 
 /**
  * @Description:
@@ -129,5 +129,96 @@ object FileUtil {
         }
     }
 
+    //    public static String unzipFile(String zipPtath, String outputDirectory)throws IOException {
+    //        /**
+    //         * 解压assets的zip压缩文件到指定目录
+    //         * @param context上下文对象
+    //         * @param assetName压缩文件名
+    //         * @param outputDirectory输出目录
+    //         * @param isReWrite是否覆盖
+    //         * @throws IOException
+    //         */
+    //
+    //        // 创建解压目标目录
+    //        File file = new File(outputDirectory);
+    //        // 如果目标目录不存在，则创建
+    //        if (!file.exists()) {
+    //            file.mkdirs();
+    //        }
+    //        // 打开压缩文件
+    //        InputStream inputStream = new FileInputStream(zipPtath); ;
+    //        ZipInputStream zipInputStream = new ZipInputStream(inputStream);
+    //
+    //        // 读取一个进入点
+    //        ZipEntry zipEntry = zipInputStream.getNextEntry();
+    //        // 使用1Mbuffer
+    //        byte[] buffer = new byte[1024 * 1024];
+    //        // 解压时字节计数
+    //        int count = 0;
+    //        // 如果进入点为空说明已经遍历完所有压缩包中文件和目录
+    //        while (zipEntry != null) {
+    //            if (!zipEntry.isDirectory()) {  //如果是一个文件
+    //                // 如果是文件
+    //                String fileName = zipEntry.getName();
+    //                Log.i("qianjujun","解压文件 原来 文件的位置： " + fileName);
+    //                fileName = fileName.substring(fileName.lastIndexOf("/") + 1);  //截取文件的名字 去掉原文件夹名字
+    //                Log.i("qianjujun","解压文件 的名字： " + fileName);
+    //                file = new File(outputDirectory + File.separator + fileName);  //放到新的解压的文件路径
+    //
+    //                file.createNewFile();
+    //                FileOutputStream fileOutputStream = new FileOutputStream(file);
+    //                while ((count = zipInputStream.read(buffer)) > 0) {
+    //                    fileOutputStream.write(buffer, 0, count);
+    //                }
+    //                fileOutputStream.close();
+    //
+    //            }
+    //
+    //            // 定位到下一个文件入口
+    //            try {
+    //                zipEntry = zipInputStream.getNextEntry();
+    //            }catch (Exception e){
+    //                e.printStackTrace();
+    //                Log.e("qianjujun", "unzipFile: ", e);
+    //            }
+    //
+    //            Log.i("qianjujun","解压文件 入口 2： " + zipEntry );
+    //        }
+    //        zipInputStream.close();
+    //        Log.i("qianjujun","解压完成");
+    //        return outputDirectory;
+    //    }
+
+    fun unZip(zipFile: File, outDir: String?): File {
+        var name = zipFile.name
+        name = name.replace(".zip".toRegex(), "")
+        val outFileDir = File(outDir, name)
+        if (!outFileDir.exists()) {
+            outFileDir.mkdirs()
+        }
+        val zip = ZipFile(zipFile)
+        val enumeration: Enumeration<*> = zip.entries()
+        while (enumeration.hasMoreElements()) {
+            val entry: ZipEntry = enumeration.nextElement() as ZipEntry
+            val zipEntryName: String = entry.getName()
+            val `in`: InputStream = zip.getInputStream(entry)
+            if (entry.isDirectory()) {      //处理压缩文件包含文件夹的情况
+                val result = File(outDir, zipEntryName)
+                result.mkdir()
+                continue
+            }
+            val file = File(outDir, zipEntryName)
+            file.createNewFile()
+            val out: OutputStream = FileOutputStream(file)
+            val buff = ByteArray(1024)
+            var len: Int
+            while (`in`.read(buff).also { len = it } > 0) {
+                out.write(buff, 0, len)
+            }
+            `in`.close()
+            out.close()
+        }
+        return outFileDir
+    }
 
 }
